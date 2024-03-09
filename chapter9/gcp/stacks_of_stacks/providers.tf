a03594c317a4ba0405ff6cb850c1e59936fa2e50
noriko <norikosama@gmail.com>
2024-03-10 01:57:29 +0900

    fix

--- chapter9/gcp/stacks_of_stacks/providers.tf
+++ chapter9/gcp/stacks_of_stacks/providers.tf
@@ -1,3 +1,13 @@
+locals {
+  # 作成したサービスアカウントを記載します
+  terraform_service_account = "terraform-gcp-account@terraform-project-410515.iam.gserviceaccount.com"
+
+  # プロジェクト名を指定します
+  project = "Terraform-project"
+
+  # regionを指定します
+  region = "asia-northeast1"
+}
 terraform {
   required_version = ">= 1.7"
   required_providers {
@@ -6,8 +16,27 @@ terraform {
     }
   }
 }
+
 provider "google" {
-  project = var.project_name
-  region  = var.region
-  zone    = var.zone
+  alias = "impersonation"
+  scopes = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
+  project = local.project
+  region  = local.region
+}
+
+data "google_service_account_access_token" "default" {
+  provider               = google.impersonation
+  target_service_account = local.terraform_service_account
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "1200s"
+}
+
+provider "google" {
+  project         = local.project
+  region          = local.region
+  access_token    = data.google_service_account_access_token.default.access_token
+  request_timeout = "60s"
 }
